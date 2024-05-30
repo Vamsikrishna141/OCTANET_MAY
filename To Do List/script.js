86fe6a46e95e54039da048ab867dcd20cb167c0c
Vamsikrishna141 <vamsikrishna.svk777@gmail.com>
2024-05-30 21:58:11 +0530

    Add files via upload

---
+++ To Do List/script.js
@@ -0,0 +1,22 @@
+function getAndUpdate() {
+    console.log("Updating List...");
+    
+}
+
+function update() {
+    
+}
+
+add = document.getElementById("add");
+add.addEventListener("click", getAndUpdate);
+
+update();
+
+function deleted(itemIndex) {
+
+}
+
+
+function clearStorage() {
+    
+}
